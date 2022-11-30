f22f3387edd57588b98af944dce7ff38008e28ce
mdmominul-310 <mdmominulislam310@gmail.com>
2022-11-30 11:20:07 +0600

    animation

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import logo from './img/logo.png'
 import 'animate.css';
 import SliderCard from './sliderCard/SliderCard';
 import Footer from './Footer/Footer';
+import { Animator, ScrollPage, Fade, batch, Move, Sticky, ScrollContainer, MoveOut } from 'react-scroll-motion';
 
 
 
@@ -15,28 +16,33 @@ import Footer from './Footer/Footer';
 
 
 function App() {
+  const FadeUp = batch(Fade(), Sticky(), MoveOut(0, -230));
 
   return (
     <React.Fragment>
       <div className="App">
-
         <div className='image-container'>
           <Navigation />
-          <div className='d-flex justify-content-center align-items-center h-100'>
-            <div>
-              <h1 className='text-light' >Start something epic.  </h1>
-              <div className='d-flex  justify-content-center'>
-                <button className='btn-primary '>
-                  Create account
-                </button>
-              </div>
-
-            </div>
+          <div className='d-flex justify-content-center align-items-center w-100'>
+            <ScrollContainer>
+              <ScrollPage>
+                <Animator animation={FadeUp}>
+                  <div>
+                    <h1 className='text-light' >Start something epic.  </h1>
+                    <div className='d-flex  justify-content-center'>
+                      <button className='btn-primary '>
+                        Create account
+                      </button>
+                    </div>
+                  </div>
+                </Animator>
+              </ScrollPage>
+            </ScrollContainer>
           </div>
         </div>
+        <SliderCard />
+        <Footer />
       </div>
-      <SliderCard />
-      <Footer />
       <CustomPreloader background="linear-gradient( #fd267a, #ff6036)" animation="fade">
         <div>
           <img src={logo} className="logo-img animate__animated  animate__zoomIn animate__infinite	infinite" />
